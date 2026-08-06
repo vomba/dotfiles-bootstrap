5d50a1b00d521cec48fa3d4fc20daaadbcfabb5b
vomba <harzallahhani@gmail.com>
2026-08-06 16:44:54 +0200

    fix(shell): use programs.delta HM API
    programs.git.delta.enable was renamed; set programs.delta.enable and
    enableGitIntegration explicitly to clear HM deprecation warnings.

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -8,11 +8,15 @@
   config = lib.mkIf config.dotfiles.shell.git.enable {
     programs.git = {
       enable = true;
-      delta.enable = true;
       settings = {
         url."ssh://git@github.com/".insteadOf = "https://github.com/";
         diff.sopsdiffer.textconv = "sops -d";
       };
+    };
+
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = true;
     };
 
     home.packages = [
