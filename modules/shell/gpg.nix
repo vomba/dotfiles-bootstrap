765480f7f0d6f428c9c6286c0a9e519e4f6f452e
vomba <harzallahhani@gmail.com>
2026-08-06 14:43:00 +0200

    feat(shell): manage gnupg via HM on Linux and Darwin
    Enable programs.gpg and gpg-agent on both platforms. Use pinentry-qt
    on Linux (Cursor-safe GUI) and pinentry_mac on Darwin; drop the
    duplicate zsh gpg launch block in favor of HM integrations.

--- modules/shell/gpg.nix
+++ modules/shell/gpg.nix
@@ -6,28 +6,28 @@
 }:
 let
   isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
 in
 {
   config = lib.mkIf config.dotfiles.shell.gpg.enable {
     programs.gpg = {
-      enable = isDarwin;
+      enable = true;
       settings.trust-model = "tofu+pgp";
     };
 
     services.gpg-agent = {
-      enable = isDarwin;
+      enable = true;
       enableSshSupport = true;
       enableZshIntegration = true;
-      pinentry.package = if isDarwin then pkgs.pinentry_mac else null;
+      pinentry.package =
+        if isDarwin then
+          pkgs.pinentry_mac
+        else if isLinux then
+          pkgs.pinentry-qt
+        else
+          null;
     };
 
-    home.packages = lib.optional isDarwin pkgs.pinentry_mac;
-
-    programs.zsh.initContent = ''
-      export GPG_TTY="$(tty)"
-      unset SSH_AGENT_PID
-      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-      gpgconf --launch gpg-agent
-    '';
+    home.packages = lib.optional isDarwin pkgs.pinentry_mac ++ lib.optional isLinux pkgs.pinentry-qt;
   };
 }
